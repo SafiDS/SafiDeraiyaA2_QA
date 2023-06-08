da245f00c1062ffacddbe819cc6d64fa079000a2
Safi Deraiya <safideraiya7@gmail.com>
2023-06-07 23:44:02 -0400

    Equilateral Triangle Test Case Completed

---
+++ SafiDeraiyaA2.nunitTests/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using System.Runtime.Intrinsics.X86;
+using TriangleSolver;
+
+namespace SafiDeraiyaA2.nunitTests;
+
+public class Tests
+{
+    private Triangle triangleSolver { get; set; } = null!;
+
+    [SetUp]
+    public void Setup()
+    {
+        triangleSolver = new Triangle();
+    }
+
+    //   Only one(1) test for a valid equilateral triangle
+
+
+    [Test]
+    public void AnalyzeTriangle_Input12and12and12_OutputvalidEquilateralTriangle()
+    {
+        //Arrange
+        int firstAngle = 12;
+        int secondAngle = 12;
+        int thirdAngle = 12;
+
+        string expected = "The triangle is valid and is an EQUILATERAL";
+
+        //Act
+        string actual = Triangle.AnalyzeTriangle(firstAngle, secondAngle, thirdAngle);
+
+        //Assert
+        Assert.AreEqual(expected, actual);
+    }
+
+
+}
+
+
+
+
+
